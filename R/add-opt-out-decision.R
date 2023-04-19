f53dbd881f33ba898c7dfa1de22fcd92638f0342
jenandre <jandre@urban.org>
2023-04-19 13:28:41 -0400

    add opt out prob and decision functions

---
+++ R/add-opt-out-decision.R
@@ -0,0 +1,18 @@
+#' Add opt out decision to starting data
+#'
+#' @param starting_data A dataframe of starting data
+#' @param opt_out_prob Name of opt out probability column
+#' @param threshold Probability threshold
+#' 
+#' @return A dataframe of starting data with opt out decision added
+#'
+add_opt_out_decision <- function(starting_data, 
+                                 opt_out_prob,
+                                 threshold){
+
+  new_df <- starting_data |>
+    dplyr::mutate(opt_out_decision = ifelse(opt_out_prob >= threshold, 1, 0))
+  
+  return(new_df)
+  
+}
---
+++ R/add-opt-out-prob.R
@@ -0,0 +1,21 @@
+#' Add probability of opt out to starting data
+#'
+#' @param starting_data A dataframe of starting data
+#' 
+#' @return A dataframe of starting data with probability column added
+#'
+add_opt_out_prob <- function(starting_data){
+  
+  households <- starting_data |>
+    dplyr::select(serialno) |>
+    dplyr::distinct()
+  
+  households$opt_out_prob <- runif(nrow(households))
+  
+  new_df <- starting_data |>
+    dplyr::full_join(households)
+  
+  return(new_df)
+  
+}
+  
