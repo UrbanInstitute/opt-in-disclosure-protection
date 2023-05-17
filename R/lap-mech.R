c141fb3d6c490cb6670596b444894eb3432ca4d1
jenandre <jandre@urban.org>
2023-05-17 14:52:18 -0400

    update global approach with lap_mech function

---
+++ R/lap-mech.R
@@ -0,0 +1,24 @@
+#' Laplace mechanism
+#'
+#' @param n The number of draws
+#' @param eps epsilon, privacy loss (num)
+#' @param gs L1 global sensitivity of the specific query (num)
+#'
+#' @return A vector of draws from the lap. dist. that satisfies approx-DP (num)
+#' 
+lap_mech <- function(n, epsilon, gs) {
+  
+  # Calculating the scale
+  scale <- gs / eps
+  
+  r <- stats::runif(n)
+  
+  x <- ifelse(
+    test = r > 0.5,
+    yes = 0 - sign(r - 0.5) * scale * log(2 * (1 - r)),
+    no = 0 - sign(r - 0.5) * scale * log(2 * r)
+  )
+  
+  return(x)
+  
+}
