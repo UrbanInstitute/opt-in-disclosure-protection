8aa2b72ffc7cc2e2fcfae91d3dc50aea8c67f594
jenandre <jandre@urban.org>
2023-05-17 18:19:18 -0400

    update opt in probability function to extend probability types

--- R/add-prob-opt-in.R
+++ R/add-prob-opt-in.R
@@ -1,16 +1,21 @@
 #' Add probability of opt in (household level) to starting data
 #'
 #' @param starting_data A dataframe of starting data
+#' @param prob The type of opt-in probability: NULL (default), "uniform"
 #' 
 #' @return A dataframe of starting data with probability column added
 #'
-add_prob_opt_in <- function(starting_data) {
+add_prob_opt_in <- function(starting_data, prob = NULL) {
   
   households <- starting_data |>
     dplyr::select(serialno) |>
     dplyr::distinct()
   
-  households$prob_opt_in <- runif(nrow(households))
+  if (is.null(prob)) {
+    households$prob_opt_in <- 1
+  } else if (prob == "uniform") {
+    households$prob_opt_in <- runif(nrow(households))
+  }
   
   new_df <- starting_data |>
     dplyr::full_join(households, by = "serialno")
