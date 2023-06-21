5bcb9cff016c46ef9ac1482e81bba15c31db3f3d
awunderground <aaronrwilliams100@gmail.com>
2023-06-21 16:17:36 -0400

    Fix param description for opt in probability

--- R/add_opt_in.R
+++ R/add_opt_in.R
@@ -1,7 +1,7 @@
 #' Add opt in decision to starting data
 #'
 #' @param starting_data A dataframe of starting data
-#' @param prob_opt_in Name of opt in probability column
+#' @param prob_opt_in A number in \[0, 1\] for the probability of opting in
 #' @param threshold Probability threshold
 #' 
 #' @return A dataframe of starting data with opt in decision added
